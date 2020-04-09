89ccd70de0c491cfcd01a3f05a5b06f61df2e993
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-04-09 19:34:14 +0530

    k

--- bot.py
+++ bot.py
@@ -1,11 +1,44 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 import config
-from commands import start
 from commands.find import search_query
 from commands.buttonhandler import button
+
+def start(bot, update):
+    chat_id = update.message.chat.id
+    keyboard = [[
+        InlineKeyboardButton('Support Chat',
+                             url=config.supportChatUrl)
+    ],
+        [
+            InlineKeyboardButton('Repo Link',
+                                 url=config.appUrl)
+        ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.sendMessage(chat_id, "<b>Hi, I Can Search Torrent Database For Your Query.</b>\n\n"
+                             "Supports Inline Mode \n-/help For More Info\n",
+                    parse_mode='HTML',
+                    reply_markup=reply_markup)
+
+def help(bot, update):
+    chat_id = update.message.chat.id
+    keyboard = [[
+        InlineKeyboardButton('Support Chat',
+                             url=config.supportChatUrl)
+    ],
+        [
+            InlineKeyboardButton('Repo Link',
+                                 url=config.appUrl)
+        ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.sendMessage(chat_id, "<b>Available commands are: </b>\n" +
+                    "-/find - <code>Searches Torrent Database</code>\n"
+                    "Ask/Report In Support Group If You Have Any Problem With This Bot. Kthnxbye",
+                    parse_mode='HTML',
+                    reply_markup=reply_markup)
 
 def main():
     updater = Updater(config.botToken)
@@ -21,6 +54,7 @@ def main():
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
     dp.add_handler(CallbackQueryHandler(button))
+    
     updater.start_polling()
     logger.info('Listening humans as %s..' % updater.bot.username)
     updater.idle()
