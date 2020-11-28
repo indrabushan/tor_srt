92a91e6371b7798a7c3842743e235e7b37997557
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2020-11-28 17:27:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ apiKey = "halyoa"
 # API KEY FOR THE API IS PUBLIC
 # DON'T ABUSE THE API
 apiParams = f"&sort=0&category=0&page=0&adult=0&key={apiKey}&concurrent=0&provider_ids[]=32"
-botToken = '<YOUR BOT API KEY HERE>'
+botToken = '1401031946:AAH5sZvNlf4H7l_8R5cNj_MDnytdQzjUXcY'
 supportChatUrl = "https://t.me/Torrent_To_Files"
 appUrl = 'https://t.me/SI_NN_ER_LS'
