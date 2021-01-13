93133d988aa2ef0e5e6931cb29cc23fea79b1b92
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2021-01-13 00:39:27 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-apiUrl = "http://157.230.67.147/t_api/simplehtmldom_1_5/my_parsers/scraping/my_scraper3.php?query="
-apiKey = "halyoa"
+apiUrl = "https://api.sumanjay.cf/torrent/?query="
+apiKey = ""
 # API KEY FOR THE API IS PUBLIC
 # DON'T ABUSE THE API
 apiParams = f"&sort=0&category=0&page=0&adult=0&key={apiKey}&concurrent=0&provider_ids[]=32"
