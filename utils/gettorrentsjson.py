6cadb2648b0d58899a1da007d7b8537a08858353
indrabushan <72606082+indrabushan@users.noreply.github.com>
2021-03-28 14:20:03 +0530

    Update gettorrentsjson.py

--- utils/gettorrentsjson.py
+++ utils/gettorrentsjson.py
@@ -3,5 +3,5 @@ import requests
 
 def returnResult(query):
     url = "https://api.sumanjay.cf/torrent/?query={}"
-    result = requests.get(url)
+    result = requests.get(url).json()
     return result.json()['results']
